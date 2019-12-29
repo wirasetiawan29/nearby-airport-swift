dd1f6a77c957d4dff157ec17df6d2bf68479d4b5
wira <wirasetiawan29@gmail.com>
2019-12-29 18:49:38 +0700

    init

---
+++ NearbyAirport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearbyAirport
+//
+//  Created by wira on 12/29/19.
+//  Copyright © 2019 Wira Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
